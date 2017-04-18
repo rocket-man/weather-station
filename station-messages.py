a796bab37a19b0bd93d98e8e8c64d4cc13aba374
Julian Claxton <jluca00@gmail.com>
2017-04-18 18:45:13 -0400

    Add file back

---
+++ station-messages.py
@@ -0,0 +1,146 @@
+"""
+Weather station messages:
+This sends a daily report from the weather station along with when the next launch is.
+It also sends launch alerts five minutes before a launch with a link to view a livestream
+of the launch.
+"""
+
+from forecastiopy import *
+import time
+import urllib.request as request
+import json
+import time
+import datetime
+import os
+import smtplib
+
+SI_UNITS = True
+USERNAME = "EMAIL_USERNAME"
+PASSWORD = "EMAIL_PASSWORD"
+TO_ADD = "RECIPIENT_EMAIL" # Use an SMS gateway to send texts
+FROM_ADD = "SENDER_EMAIL"
+
+# Sends report at specified time and launch alerts five minutes before window opening
+def main():
+	while True:
+		rawHour, rawMinute = getTime()
+		hour = int(rawHour)
+		minute = int(rawMinute)
+		
+		os.system('clear')
+		print(hour, ":", minute)
+		
+		launchName, windOpens, temp, summary, vid = getInfo()
+		if vid == "":
+			vid = "No stream found"
+			
+		window = windOpens[-12:-7]
+		# windowHour and Minute are used for comparison to send alert, windOpens is for use in message
+		windowHour = int(window[:2]) - 4 # To convert UTC to EST
+		windowMinute = int(window[-2:])
+		windOpens = windOpens[:-19] # Remove UTC time and year
+		# Launch alert
+		if hour == windowHour and minute == windowMinute - 5:
+			message = "\nLaunch window for %s opening in five minutes. Watch live: %s" %(
+						launchName, vid)
+			send(message, "")
+			print("Launch alert sent.")
+		# Timed message
+		if hour == 5 and minute == 0:			
+			message = makeMessage(launchName, windOpens, temp, summary, windowHour, windowMinute)
+			messageOne, messageTwo = splitMessage(message)			
+			send(messageOne, messageTwo)
+			print("Message sent.")
+		
+		else:
+			pass
+	
+		time.sleep(60)
+
+# Retrieves current and daily weather forecasts and date/name of next launch
+def getInfo():
+	apikey = 'API_KEY'
+	home = [YOUR_LAT, YOUR_LONG] # lat long
+	# Instantiates ForecastIO class that will be passed to currently and daily
+	if SI_UNITS:
+		fio = ForecastIO.ForecastIO(apikey, units = ForecastIO.ForecastIO.UNITS_SI,
+									lang = ForecastIO.ForecastIO.LANG_ENGLISH,
+									latitude = home[0], longitude = home[1]
+									)
+	else:
+		fio = ForecastIO.ForecastIO(apikey, units = ForecastIO.ForecastIO.UNITS_US,
+									lang = ForecastIO.ForecastIO.LANG_ENGLISH,
+									latitude = home[0], longitude = home[1]
+									)	
+	current = FIOCurrently.FIOCurrently(fio)
+	currentTemp = current.temperature # Will be replaced w/ station data
+	
+	daily = FIODaily.FIODaily(fio)
+	summ = daily.summary
+	weeklySum = summ.translate(str.maketrans('','', '\xb0')) # Remove degree symbol
+																
+	launchResponse = request.urlopen("https://launchlibrary.net/1.2/launch/next/1").read().decode('utf8')
+	launchInfo = json.loads(launchResponse)
+	
+	name = launchInfo['launches'][0]['name']
+	windowOpen = launchInfo['launches'][0]['windowstart']
+	link = str(launchInfo['launches'][0]['vidURLs']).strip("[']") # Get link and remove quotes
+	
+	return name, windowOpen, currentTemp, weeklySum, link
+
+# Retrieves and returns current hour and minute
+def getTime():
+	t = datetime.datetime.now()
+
+	hr = t.hour
+	min = t.minute
+	
+	return hr, min
+
+# This assembles all the information into the final form to be sent in the text(s)
+def makeMessage(LName, LWind, WCurrent, WSummary, windowHr, windowMin):
+	if SI_UNITS:
+		units = "C"
+	else:
+		units = "F"
+		
+	if windowHr < 0: # If time is 00:00 UTC, conversion to EST makes it negative
+		windowHr = 0
+	windowHr = str(windowHr)
+	windowMin = str(windowMin)
+	if windowHr == "0": # Add 0s for time readability 
+		windowHr = "00"
+	if windowMin == "0":
+		windowMin = "00"
+
+	text = "The date is %s and the temperature is %s %s. Expect %s The next launch is %s on %s at %s:%s." %(
+			getDate(), WCurrent, units, WSummary, LName, LWind, windowHr, windowMin)
+	return text
+
+# Gets the current date and formats it to be easily readable
+def getDate():
+	now = datetime.datetime.now()
+	day = now.day
+	month = now.month
+	year = now.year
+	
+	todayDate = datetime.date(day = day, month = month, year = year).strftime('%A, %B %d, %Y')
+	return todayDate
+
+# Splits the message into two since it exceeds character limit of SMS
+def splitMessage(mess):
+	messOne = '\n' + mess[0:140]
+	messTwo = '\n' + mess[140:]
+	return messOne, messTwo
+
+# Sends an email
+def send(textOne, textTwo):
+	server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+	server.login(USERNAME, PASSWORD)
+	server.sendmail(FROM_ADD, TO_ADD, textOne)
+	if textTwo != "":
+		server.sendmail(FROM_ADD, TO_ADD, textTwo)
+	server.quit
+	del server
+
+main()
